8f180f201a167d5fc854d259f82b5726862deb91
Santosh.Sanwal <santoshkumarsanwal@gmail.com>
2019-03-19 11:16:49 +0530

    added requirements file

--- blog/models.py
+++ blog/models.py
@@ -6,3 +6,13 @@ class blog(models.Model):
     pubdate=  models.DateTimeField()
     body = models.TextField()
     image = models.ImageField(upload_to='images/')
+
+
+    def summary(self):
+        return self.body[:500]
+
+    def pub_date_pretty(self):\
+        return self.pubdate.strftime('%b %e %Y')
+
+    def __str__(self):
+        return self.title
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
+from .models import blog
+
+
+def allblogs(request):
+    blogs = blog.objects
+    return render(request, 'blog/allblog.html', {'blogs': blogs})
+
+def detail(request, blog_id):
+     detailblog=get_object_or_404(blog, pk=blog_id)
+     return render(request, 'blog/detail.html', {'blog':detailblog})
